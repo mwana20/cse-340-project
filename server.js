68d6b636bc96e72c91e8daea1c3e1cd52bb4372b
Mubbala Phillip Kenneth <phillipmwanaweika8@gmail.com>
2025-05-29 17:33:21 +0300

    y

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
